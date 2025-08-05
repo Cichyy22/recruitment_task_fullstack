0ee06479c8f86c58be13966566b765dc3141cc41
Cichyy22 <ok.kuba@wp.pl>
2025-08-05 12:39:48 +0200

    feat: end core features of app

--- assets/js/components/Home.js
+++ assets/js/components/Home.js
@@ -1,8 +1,9 @@
 // ./assets/js/components/Home.js
 
 import React, {Component} from 'react';
-import {Route, Redirect, Switch, Link} from 'react-router-dom';
+import {Route, Switch, Link} from 'react-router-dom';
 import SetupCheck from "./SetupCheck";
+import ExchangePage from "./ExchangePage";
 
 class Home extends Component {
 
@@ -10,18 +11,20 @@ class Home extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <Link className={"navbar-brand"} to={"#"}> Telemedi Zadanko </Link>
+                    <Link className={"navbar-brand"} to={"#"}> System kantoru $$ </Link>
                     <div id="navbarText">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
+                                <Link className={"nav-link"} to={"/"}> Kursy walut </Link>
+                            </li>
+                            <li className="nav-item">
                                 <Link className={"nav-link"} to={"/setup-check"}> React Setup Check </Link>
                             </li>
-
                         </ul>
                     </div>
                 </nav>
                 <Switch>
-                    <Redirect exact from="/" to="/setup-check" />
+                    <Route exact path="/" component={ExchangePage} />
                     <Route path="/setup-check" component={SetupCheck} />
                 </Switch>
             </div>
